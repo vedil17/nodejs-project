c73e4eeb246b10839813706a9833b8ab9f859ce6
vedil <alouan_adil@hotmail.fr>
2022-11-15 05:45:52 +0100

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1373238643209365',
+        clientSecret: '32cc5ddb07f69c3473ebe182218cef93'
+    }
 }
